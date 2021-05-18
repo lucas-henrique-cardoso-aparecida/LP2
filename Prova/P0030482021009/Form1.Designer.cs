7dc7583184a3eaa595f61e68e355e72bdc4e99a2
lucas-henrique-cardoso-aparecida <78798597+lucas-henrique-cardoso-aparecida@users.noreply.github.com>
2021-05-18 12:10:09 -0300

    Add files via upload

---
+++ Prova/P0030482021009/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace P0030482021009
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lbxTotal = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(60, 43);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(146, 94);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lbxTotal
+            // 
+            this.lbxTotal.FormattingEnabled = true;
+            this.lbxTotal.Location = new System.Drawing.Point(263, 12);
+            this.lbxTotal.Name = "lbxTotal";
+            this.lbxTotal.Size = new System.Drawing.Size(264, 420);
+            this.lbxTotal.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.lbxTotal);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lbxTotal;
+    }
+}
+
---
+++ Prova/P0030482021009/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482021009
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int semanas = 4;
+            int meses = 9;
+            double total = 0;
+
+            double[,] Valores = new double[meses,semanas];
+
+            double[] valormes = new double[meses];
+            double[] valorsemana = new double[semanas];
+            
+
+            for (int i = 0; i < meses; i ++)
+            {
+               for (int j = 0; j < semanas; j++ )
+                {
+                    var leitura = Interaction.InputBox($"Total Semana #{j + 1}", $"Valores Mês {i + 1}");
+
+                    if (!double.TryParse(leitura, out Valores[i,j]))
+                    {
+                        MessageBox.Show("Erro", "Atenção");
+                        j--;
+                        continue;
+                    }
+
+                    valorsemana[j] = Valores[i, j];
+                    valormes[i] += Valores[i, j];
+
+                    lbxTotal.Items.Add($"Total do Mês:{i + 1} Semana{j + 1}: {valorsemana[j].ToString("C2")}");
+                }
+                total = total + valormes[i];
+
+                lbxTotal.Items.Add($"-------------------");
+                lbxTotal.Items.Add($">> Total Mês: {valormes[i].ToString("C2")}");
+                lbxTotal.Items.Add($"-------------------");
+            }
+            lbxTotal.Items.Add($"Total Geral: {total.ToString("C2")}");
+        }
+    }
+}
